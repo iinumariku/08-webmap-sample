637e11de3fc1e5f3acc25539eee039cc4dc2e8a9
kamataryo <mugil.cephalus+github@gmail.com>
2025-11-17 20:30:55 +0900

    Finish

--- index.js
+++ index.js
@@ -13,43 +13,74 @@ const map = new maplibregl.Map({
       sources: {
         'gsi-pale': {
           type: 'raster',
-          // [カスタマイズ]
-          // 以下のサイトを確認して、地図の種類を変えてみよう
-          // std -> pale, english, ...
           // https://maps.gsi.go.jp/development/ichiran.html
           tiles: ['https://cyberjapandata.gsi.go.jp/xyz/std/{z}/{x}/{y}.png'],
           tileSize: 256,
           attribution: '<a href="https://maps.gsi.go.jp/development/ichiran.html" target="_blank">地理院タイル</a>',
+        },
+        'my-data': {
+          type: 'geojson',
+          data: 'sample.geojson',
         }
       },
-      layers: [{
-        id: 'gsi-pale',
-        type: 'raster',
-        source: 'gsi-pale',
-      }]
+      layers: [
+        {
+          id: 'gsi-pale',
+          type: 'raster',
+          source: 'gsi-pale',
+        },
+        {
+          id: 'my-data_circle-layer',
+          type: 'circle',
+          source: 'my-data',
+          paint: {
+            'circle-radius': 6,
+            'circle-color': '#FF0000',
+            'circle-stroke-width': 2,
+            'circle-stroke-color': '#FFFFFF',
+          },
+        },
+        {
+          id: 'my-data_symbol-layer',
+          type: 'symbol',
+          source: 'my-data',
+          layout: {
+            'text-field': ['get', 'name'], // GeoJSONのプロパティ'name'を表示
+            'text-font': ['Noto Sans JP Regular'],
+            'text-size': 12,
+            'text-offset': [0, 1.5],
+            'text-anchor': 'top',
+          },
+          paint: {
+            'text-color': '#000000',
+            'text-halo-color': '#FFFFFF',
+            'text-halo-width': 2,
+          }
+        }
+      ]
     },
 
     // center: 地図の初期表示位置を経度・緯度で指定
-    // [経度, 緯度] の順番で指定します（注意：緯度・経度ではありません）
-    // 【カスタマイズ】日本の色々な場所を中心にしてみよう
+    // [経度, 緯度] の順番で指定します
     center: [139.6917, 35.6895], // 東京駅付近
 
     // zoom: 地図の初期ズームレベルを指定
     // 数値が大きいほど拡大表示されます
-    // 【カスタマイズ】数字を変えてズームを調整してみよう
     zoom: 1,
 
-    // 【カスタマイズ】 false -> true にすると？
-    hash: false,
+    // hash: URLのハッシュ部分に地図の位置情報を反映させるかどうか
+    hash: true,
+
+    maxZoom: 10,
+    minZoom: 4,
 });
 
-// 地図がクリックされたときの処理
-// 'click'イベントは地図上のどこかがクリックされたときに発生します
-map.on('click', function(e) {
-    // e.lngLatにはクリックした位置の経度・緯度が含まれています
-    const longitude = e.lngLat.lng; // 経度を取得
-    const latitude = e.lngLat.lat;  // 緯度を取得
+// クリックした時のインタラクション
+map.on('click', function (e) {
+    const features = map.queryRenderedFeatures(e.point, { layers: ['my-data_circle-layer', 'my-data_symbol-layer'] });
+    if(features.length > 0){
+        const feature = features[0];
+        alert('クリックした地物:\n' + feature.properties.name);
+    }
+})
 
-    // アラートでクリック位置を表示（小数点以下4桁まで表示）
-    alert(`クリックした位置:\n経度: ${longitude.toFixed(4)}\n緯度: ${latitude.toFixed(4)}`);
-});
